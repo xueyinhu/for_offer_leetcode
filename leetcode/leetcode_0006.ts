f6439c44411f529b4385e18e82cee19dea5cb2af
xueyinhu <13453894389@163.com>
2023-08-14 23:47:42 +0800

    that

---
+++ leetcode/leetcode_0006.ts
@@ -0,0 +1,11 @@
+/**  N 字形变换
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ * 
+ * 1 <= s.length <= 1000
+ * s 由英文字母（小写和大写）、',' 和 '.' 组成
+ * 1 <= numRows <= 1000
+ */
+
+function convert(s: string, numRows: number): string {
+
+};
